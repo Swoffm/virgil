f8fc02eb8f5c38165f1deb652434feb913da5c37
Gavin Swofford <gavinswofford@gmail.com>
2020-10-27 14:51:08 -0500

    fixed several bugs

--- VirgilWebApi/VirgilWebApi/client/src/App.js
+++ VirgilWebApi/VirgilWebApi/client/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import UserProfileManager from "./components/apiManager/UserProfileApi";
-import AppView from "./components/AppView";
+// import AppView from "./components/AppView";
+import AppView from "./components/appView";
 import AppNav from "./components/nav/Nav";
 
 
