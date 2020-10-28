ab7b81a6a5d9be9d6a43d0a6912f83e189639b82
Gavin Swofford <gavinswofford@gmail.com>
2020-10-28 14:38:21 -0500

    category repo and interface of the repo have been created

---
+++ VirgilWebApi/VirgilWebApi/Repositories/CategoryRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VirgilWebApi.Model;
+
+namespace VirgilWebApi.Repositories
+{
+    public class CategoryRepository : BaseRepository, ICategoryRepository
+    {
+        public CategoryRepository(IConfiguration configuration) : base(configuration) { }
+
+        public void CreateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VirgilWebApi/VirgilWebApi/Repositories/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VirgilWebApi.Model;
+
+namespace VirgilWebApi.Repositories
+{
+    public interface ICategoryRepository
+    {
+        List<Category> GetAll(int userId);
+        Category GetById(int categoryId);
+        public void CreateCategory(Category category);
+
+        public void UpdateCategory(Category category);
+        public void DeleteCategory(int categoryId);
+    }
+}
