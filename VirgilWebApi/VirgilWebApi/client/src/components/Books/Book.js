a0c8dd17500d69fb1a01541964935c8838dd04cf
Gavin Swofford <gavinswofford@gmail.com>
2020-11-03 09:04:20 -0600

    users can now filter based on category

--- VirgilWebApi/VirgilWebApi/client/src/components/Books/Book.js
+++ VirgilWebApi/VirgilWebApi/client/src/components/Books/Book.js
@@ -3,11 +3,16 @@ import { BookContext } from "../apiManager/BookApi";
 import { Button } from "reactstrap";
 import {Link} from "react-router-dom";
 import BookCard from "./BookCard";
+import {CategoryContext} from "../apiManager/CategoryApi";
+
 
 
 const Book = () => {
 
 const {getAllBooks, deleteBook, book} = useContext(BookContext);
+const [userSelect, setUserSelect] = useState();
+const {getAllCategories, deleteCategories, category} = useContext(CategoryContext);
+
 
 const userId = sessionStorage.getItem("id");
 
@@ -22,9 +27,22 @@ const deleteBookFromList = (id) => {
 
   }
 
+
+  const handleFieldChange = evt => {
+    const stateToChange = { ...userSelect };
+    stateToChange[evt.target.id] = evt.target.value;
+    setUserSelect(stateToChange);
+    console.log(userSelect)
+};
+
+
+
+
 useEffect(()=> {
 
 getAllBooks(parseInt(userId));
+getAllCategories(userId);
+
 
 }, [])
 return (
@@ -36,6 +54,10 @@ return (
       <Button color="primary">Create</Button>
       </Link>
     </p>
+    <select name="name" onClick={handleFieldChange} id="name">
+                                <option value="NA">Select a value</option>
+    {category.map(element => <option key={element.id} value={element.id}>{element.name}</option>)}
+                            </select>
     <table className="table table-striped">
       <thead>
         <tr>
@@ -44,9 +66,13 @@ return (
         </tr>
       </thead>
       <tbody>
-        {book && book.map((element) => (
+      { userSelect !== undefined ? book.map((element) => ( element.categoryId == userSelect.name || userSelect.name == "NA" ? 
+           <BookCard key={element.id} delete={deleteBookFromList} book={element} /> 
+       
+            : null ) ) : book.map((element) => ( <BookCard key={element.id} delete={deleteBookFromList} book={element} /> ))  }
+        {/* {book && book.map((element) => (
           <BookCard key={element.id} col={false} book={element} delete={deleteBookFromList}/>
-        ))}
+        ))} */}
       </tbody>
     </table>
   </div>
